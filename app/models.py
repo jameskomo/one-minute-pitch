a4e7a201089d43de002fa26373c2d2d935deaef5
jameskomo <james.komoh@gmail.com>
2019-04-22 21:48:53 +0300

    Pitch model class created in views. __tablename__ class variable used to define the name of the table in the database. Class variables defined and relationships mapped to upvote, downvote and comments tables

--- app/models.py
+++ app/models.py
@@ -15,14 +15,14 @@ def load_user(user_id):
 class User(UserMixin,db.Model):
     
     __tablename__ = 'users'
-    # id = db.Column(db.Integer, primary_key = True)
-    # username = db.Column(db.String(255))
-    # email = db.Column(db.String(255),unique = True,index = True)
-    # pass_secure = db.Column(db.String(255))
-    # pitch = db.relationship('Pitch', backref='user', lazy='dynamic')
-    # comment = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
-    # upvotes = db.relationship('Upvote', backref = 'user', lazy = 'dynamic')
-    # downvotes = db.relationship('Downvote', backref = 'user', lazy = 'dynamic')
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    pass_secure = db.Column(db.String(255))
+    pitch = db.relationship('Pitch', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
+    upvotes = db.relationship('Upvote', backref = 'user', lazy = 'dynamic')
+    downvotes = db.relationship('Downvote', backref = 'user', lazy = 'dynamic')
 
 
     @property
@@ -44,18 +44,18 @@ class User(UserMixin,db.Model):
 class Pitch(db.Model):
     '''
     '''
-    __tablename__ = 'pitches'
+    # __tablename__ = 'pitches'
 
-    id = db.Column(db.Integer, primary_key = True)
-    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
-    description = db.Column(db.String(), index = True)
-    title = db.Column(db.String())
-    # downvotes = db.Column(db.Integer, default=int(0))
-    # upvotes = db.Column(db.Integer, default=int(0))
-    category = db.Column(db.String(255), nullable=False)
-    comments = db.relationship('Comment',backref='pitch',lazy='dynamic')
-    upvotes = db.relationship('Upvote', backref = 'pitch', lazy = 'dynamic')
-    downvotes = db.relationship('Downvote', backref = 'pitch', lazy = 'dynamic')
+    # id = db.Column(db.Integer, primary_key = True)
+    # owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    # description = db.Column(db.String(), index = True)
+    # title = db.Column(db.String())
+    # # downvotes = db.Column(db.Integer, default=int(0))
+    # # upvotes = db.Column(db.Integer, default=int(0))
+    # category = db.Column(db.String(255), nullable=False)
+    # comments = db.relationship('Comment',backref='pitch',lazy='dynamic')
+    # upvotes = db.relationship('Upvote', backref = 'pitch', lazy = 'dynamic')
+    # downvotes = db.relationship('Downvote', backref = 'pitch', lazy = 'dynamic')
 
     
     @classmethod
