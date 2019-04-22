e23ebb534550d93fc7fefdae19373d9ed6eb1c36
jameskomo <james.komoh@gmail.com>
2019-04-22 22:08:14 +0300

    Registration form class created, class variables passed, equal to validator passed for password matching, required validators passed for various class attributes and validation functions declared for email and user name validation

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -7,12 +7,12 @@ from wtforms import ValidationError
 
 class RegistrationForm(FlaskForm):
 
-    # email = StringField('Your Email Address',validators=[Required(),Email()])
-    # username = StringField('Enter your username',validators = [Required()])
-    # password = PasswordField('Password',validators = [Required(),
-    # EqualTo('password_confirm',message = 'Passwords must match')])
-    # password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
-    # submit = SubmitField('Sign Up')
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(),
+    EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
 
     def validate_email(self,data_field):
             if User.query.filter_by(email =data_field.data).first():
