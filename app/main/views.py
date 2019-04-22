72693d0c197d6b1cfa344431580ed224c9c63a15
jameskomo <james.komoh@gmail.com>
2019-04-22 21:21:26 +0300

    Root view created with app.route(/) decorator.pitches initialized based on category

--- app/main/views.py
+++ app/main/views.py
@@ -8,24 +8,24 @@ from .. import db
 import markdown2
 
 
-# # Views
-# @main.route('/', methods = ['GET','POST'])
-# def index():
+# Views
+@main.route('/', methods = ['GET','POST'])
+def index():
 
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-#     pitch = Pitch.query.filter_by().first()
-#     title = 'Home'
-#     pickuplines = Pitch.query.filter_by(category="pickuplines")
-#     interviewpitch = Pitch.query.filter_by(category = "interviewpitch")
-#     promotionpitch = Pitch.query.filter_by(category = "promotionpitch")
-#     productpitch = Pitch.query.filter_by(category = "productpitch")
+    '''
+    View root page function that returns the index page and its data
+    '''
+    pitch = Pitch.query.filter_by().first()
+    title = 'Home'
+    pickuplines = Pitch.query.filter_by(category="pickuplines")
+    interviewpitch = Pitch.query.filter_by(category = "interviewpitch")
+    promotionpitch = Pitch.query.filter_by(category = "promotionpitch")
+    productpitch = Pitch.query.filter_by(category = "productpitch")
 
-#     upvotes = Upvote.get_all_upvotes(pitch_id=Pitch.id)
+    upvotes = Upvote.get_all_upvotes(pitch_id=Pitch.id)
     
 
-#     return render_template('home.html', title = title, pitch = pitch, pickuplines=pickuplines, interviewpitch= interviewpitch, promotionpitch = promotionpitch, productpitch = productpitch, upvotes=upvotes)
+    return render_template('home.html', title = title, pitch = pitch, pickuplines=pickuplines, interviewpitch= interviewpitch, promotionpitch = promotionpitch, productpitch = productpitch, upvotes=upvotes)
     
 
 
