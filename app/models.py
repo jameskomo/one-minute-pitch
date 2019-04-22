040933d356170e19aa7d0859cc1c8475375eb021
jameskomo <james.komoh@gmail.com>
2019-04-22 21:39:59 +0300

    User model class created with with attributes that match the columns of the corresponding User Table. Class variables passed as attributes of User class model, primary key defined and one to many relationship column defined(pitch)

--- app/models.py
+++ app/models.py
@@ -12,17 +12,17 @@ def load_user(user_id):
 
 
 
-# class User(UserMixin,db.Model):
+class User(UserMixin,db.Model):
     
-#     __tablename__ = 'users'
-#     id = db.Column(db.Integer, primary_key = True)
-#     username = db.Column(db.String(255))
-#     email = db.Column(db.String(255),unique = True,index = True)
-#     pass_secure = db.Column(db.String(255))
-#     pitch = db.relationship('Pitch', backref='user', lazy='dynamic')
-#     comment = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
-#     upvotes = db.relationship('Upvote', backref = 'user', lazy = 'dynamic')
-#     downvotes = db.relationship('Downvote', backref = 'user', lazy = 'dynamic')
+    __tablename__ = 'users'
+    # id = db.Column(db.Integer, primary_key = True)
+    # username = db.Column(db.String(255))
+    # email = db.Column(db.String(255),unique = True,index = True)
+    # pass_secure = db.Column(db.String(255))
+    # pitch = db.relationship('Pitch', backref='user', lazy='dynamic')
+    # comment = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
+    # upvotes = db.relationship('Upvote', backref = 'user', lazy = 'dynamic')
+    # downvotes = db.relationship('Downvote', backref = 'user', lazy = 'dynamic')
 
 
     @property
