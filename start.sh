d11891cba177879104ddfcb42d2523cca0a0f01d
jameskomo <james.komoh@gmail.com>
2019-04-29 15:04:45 +0300

    new changes

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 export SECRET_KEY=KoMoH
 export DATABASE_URL=postgresql+psycopg2://komo:KoMoH@localhost/one_minute_pitch
-
+export MAIL_USERNAME=juniorgichohi@gmail.com
+export MAIL_PASSWORD=shahidi123
 python3.6 manage.py server
